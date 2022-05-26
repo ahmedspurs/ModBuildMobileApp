66e4239379faf7d6682a11a32300d42885862a55
unknown <amnnn80@gmail.com>
2022-05-26 10:30:03 +0100

    Edit home Page

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,7 +4,7 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar>
         <!-- home page tab -->
-        <!-- <ion-tab-button tab="HomePage" href="/tabs/HomePage">
+        <ion-tab-button tab="HomePage" href="/tabs/HomePage">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-8 w-8"
@@ -20,23 +20,12 @@
             />
           </svg>
 
-        </ion-tab-button> -->
+        </ion-tab-button>
         <!-- category tab -->
         <ion-tab-button tab="CategoryPage" href="/tabs/CategoryPage">
-      <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-8 w-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-            />
-          </svg>
+   <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+</svg>
 
         </ion-tab-button>
         <!-- cart tab -->
